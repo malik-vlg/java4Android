4f21d1aab81764da375ad09fce9e10b5d56732ad
malik-vlg <abdinov-malik@mail.ru>
2021-06-10 20:27:37 +0300

    first commit

--- src/main/java/lesson8/Main.java
+++ src/main/java/lesson8/Main.java
@@ -4,9 +4,13 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Barrier[] barriers = new Barrier[4];
+
+        Barrier[] barriers = new Barrier[2];
+
         Action[] subject = new Action[10];
+
         Random random = new Random();
+
         int distance;
         int height;
 
@@ -15,9 +19,9 @@ public class Main {
             distance = random.nextInt(100);
             height = random.nextInt(10);
 
-            if(i < barriers.length/2){
+            if (i < barriers.length / 2) {
                 barriers[i] = new Treadmill(distance);
-            } else{
+            } else {
                 barriers[i] = new Wall(height);
             }
         }
@@ -25,33 +29,32 @@ public class Main {
             distance = random.nextInt(150);
             height = random.nextInt(15);
 
-            if(i < subject.length / 3){
+            if (i < subject.length / 3) {
                 subject[i] = new Human(distance, height);
-            } else if (i > subject.length / 3 && i <= (subject.length / 3) * 2){
+            } else if (i > (subject.length / 3) && i <= subject.length - (subject.length / 3)) {
                 subject[i] = new Robot(distance, height);
             } else {
                 subject[i] = new Cat(distance, height);
             }
 
-            }
+        }
         for (int i = 0; i < subject.length; i++) {
+
             System.out.println("Участник № " + i);
 
-            if(barriers[0] instanceof Treadmill){
+            if (barriers[0] instanceof Treadmill) {
                 Treadmill treadmill = (Treadmill) barriers[0];
                 subject[i].run(treadmill);
-                if(!subject[i].isResult()){
+                if (!subject[i].isResult()) {
                     continue;
                 }
-        }
-            if(barriers[i] instanceof Wall){
-                Wall wall = (Wall) barriers[i];
-                subject[i].jump(wall);
-                if(!subject[i].isResult()){
-                    continue;
+                if (barriers[i] instanceof Wall) {
+                    Wall wall = (Wall) barriers[1];
+                    subject[i].jump(wall);
+                    subject[i].isResult();
                 }
-            }
 
+            }
         }
     }
 }
