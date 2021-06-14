3d2f5f0b4b618328905494f1d8cdcea0163ee29d
malik-vlg <abdinov-malik@mail.ru>
2021-06-14 20:08:58 +0300

    first commit

---
+++ src/main/java/lesson9/Main.java
@@ -0,0 +1,49 @@
+package lesson9;
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] arrOne = new String[][]{
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+        String[][] arrTwo = new String[][]{
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"}
+        };
+
+        try{
+            transformAndSumArr(arrOne);
+        } catch (MyArraySizeException e){
+            System.out.println("Массив выведен не верно!");
+        } catch (MyArrayDataException e){
+            System.out.println("Ошибка " + e.i + e.j);
+        }
+    }
+
+    public static void  transformAndSumArr(String[][] arrOne) throws MyArrayDataException, MyArraySizeException{
+        int transform;
+        int sum = 0;
+        int maxLine = arrOne.length;
+        int maxColumn = arrOne[0].length;
+
+        if(maxLine != 4 || maxColumn != 4){
+            throw new MyArraySizeException("Размер");
+        }
+        for (int i = 0; i < arrOne.length; i++) {
+            for (int j = 0; j < arrOne[0].length; j++) {
+                try{
+                    transform = Integer.parseInt(arrOne[i][j]);
+                    sum += transform;
+                } catch (NumberFormatException e){
+                    throw new MyArrayDataException(i, j);
+                }
+
+            }
+
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/lesson9/MyArrayDataException.java
@@ -0,0 +1,14 @@
+package lesson9;
+
+public class MyArrayDataException extends Exception {
+    public int i;
+    public int j;
+
+
+    public MyArrayDataException(int x, int y){
+        this.i = i;
+        this.j = j;
+    }
+
+
+}
